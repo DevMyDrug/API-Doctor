11a47d6bc218d85070918219c4073822cc3ee195
Unknown <baptiste.desbois@outlook.com>
2017-11-12 22:43:12 +0100

    Add begin

--- DoctorAPI/DoctorAPI/Program.cs
+++ DoctorAPI/DoctorAPI/Program.cs
@@ -18,7 +18,7 @@ namespace DoctorAPI
                 string url = "https://agenda.direct/rendez-vous/medecine-generale/lyon?search=s";
                 string txt = "";
                 HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-
+                List<string> lUrl = new List<string>();
                 using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
                 using (Stream stream = response.GetResponseStream())
                 using (StreamReader reader = new StreamReader(stream))
@@ -26,11 +26,24 @@ namespace DoctorAPI
                     HtmlDocument doc = new HtmlDocument();
                     doc.LoadHtml(reader.ReadToEnd());
                     HtmlNode divList = doc.GetElementbyId("searchlist");
-                    
+                    int nbNode = 0;
+                    foreach(HtmlNode node in divList.Descendants("div"))
+                    {
+                        if(nbNode <3)
+                        {
+                            nbNode++;
+                            continue;
+                        }
+                        HtmlNode next = node.Descendants("div").ElementAt(1).Descendants("a").ElementAt(0);
+                        string nexturl = next.Attributes["href"].Value;
+                        lUrl.Add(nexturl);
+
+                    }
+
                 }
 
             }
-            catch (Exception)
+            catch (Exception ex)
             {
 
                 throw;

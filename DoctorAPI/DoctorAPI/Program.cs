d31ddc0403a32b388cfb1e7f52897e6aac01c30e
Unknown <baptiste.desbois@outlook.com>
2017-11-12 21:12:30 +0100

    Récupération HtmlDoc
    Element

--- DoctorAPI/DoctorAPI/Program.cs
+++ DoctorAPI/DoctorAPI/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using HtmlAgilityPack;
 
 namespace DoctorAPI
 {
@@ -10,6 +13,28 @@ namespace DoctorAPI
     {
         static void Main(string[] args)
         {
+            try
+            {
+                string url = "https://agenda.direct/rendez-vous/medecine-generale/lyon?search=s";
+                string txt = "";
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                using (Stream stream = response.GetResponseStream())
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    HtmlDocument doc = new HtmlDocument();
+                    doc.LoadHtml(reader.ReadToEnd());
+                    HtmlNode divList = doc.GetElementbyId("searchlist");
+                    
+                }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
     }
 }
